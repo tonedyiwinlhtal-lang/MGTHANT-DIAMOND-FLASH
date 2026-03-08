bdc8a6281e4df28ed3089ad52a502cb0507eb83d
tonedyiwinlhtal-lang <tonedyiwinlhtal@gmail.com>
2026-03-09 01:24:32 +0630

    Implement order and payment functions in script.js
    Added functions to handle product ordering and payment methods.

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-document.querySelector(".buy").onclick = () => {
+function order(product,price){
 
-alert("Order Sent! ⚡");
+document.getElementById("popup").style.display="block"
+
+document.getElementById("product").innerText=product
+document.getElementById("price").innerText=price
+
+}
+
+function pay(method){
+
+let player=document.getElementById("playerid").value
+let server=document.getElementById("serverid").value
+
+let text=`MLBB Diamond Order
+Product: ${document.getElementById("product").innerText}
+Price: ${document.getElementById("price").innerText}
+PlayerID: ${player}
+ServerID: ${server}
+Payment: ${method}`
+
+let telegram="https://t.me/YOURUSERNAME?text="+encodeURIComponent(text)
+
+window.open(telegram)
 
 }
